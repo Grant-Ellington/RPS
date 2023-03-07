c298474536847bc192562aa7c53fc53278fe05d4
Grant-Ellington <gellingtonem6@gmail.com>
2023-03-06 19:57:23 -0600

    added user choice prompt

---
+++ script.js
@@ -0,0 +1,25 @@
+// Define a function that starts the game
+function play() {
+
+    userChoice = prompt('Pick R,P,S')
+    // Ask user for their choice using a promt
+    // If user pressed Cancel, immediately end function
+    // Get the computer choice by randomly selecting "r", "s", or "p"
+    // Alert user to computer choice using an alert
+    // Compare user choice and computer choice
+    // if choices are the same, add to the ties variable and
+    // alert user that it is a tie
+    // if user wins, add to the wins variable and alert user that they won
+    // if user losses, add to losses variable and alert user that they lost
+    // alert users of their wins, losses, and ties
+    // ask user to play again, if yes call function
+  };
+  // play the game the first time
+  
+  
+  play();
+  
+  
+  
+  
+  
